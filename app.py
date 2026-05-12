77c93aa5539fffe10b202cc90a7ef9a56782766d
srivijayadharshini <srivijayadharshini.2024aids@sece.ac.in>
2026-05-12 14:40:00 +0530

    change in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Demo Successful 🚀"
+    return "hello Welcome to Sri WOrld 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
